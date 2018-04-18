2e2b5cf4e1525b4f71a69b9ece8dc2a7ce5b6061
Khanh Duc Ha <ken.haduc@gmail.com>
2018-04-18 23:00:35 +1200

    Tweak SideMenu
    Tweaked SideMenu, handle basic screen size changes
    TODO: Handle more cases

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,67 +1,63 @@
 import React, { Component } from "react"
+import styled from "styled-components"
+import MENU_CONFIG from "./MenuConfig"
 import "./SideMenu.css"
-import * as FontAwesome from "react-icons/lib/fa"
 
-const MENU = [
-  {
-    label: "Profile",
-    icon: "FaAsterisk",
-    style: {
-      top: "5%",
-    },
-  },
-  {
-    label: "Friends",
-    icon: "FaAssistiveListeningSystems",
-    style: {
-      top: "40%",
-    },
-  },
-  {
-    label: "Star",
-    icon: "FaStarO",
-    style: {
-      top: "50%",
-    },
-  },
-  {
-    label: "Conversations",
-    icon: "FaCommentsO",
-    style: {
-      top: "60%",
-    },
-  },
-  {
-    label: "Settings",
-    icon: "FaCog",
-    style: {
-      top: "85%",
-    },
-  },
-]
+const SmallItem = styled.div`
+  display: none;
+  @media (max-width: ${props => props.size}px) {
+    display: block;
+  }
+`
+const BigItem = styled.div`
+  display: block;
+  @media (max-width: ${props => props.size}px) {
+    display: none;
+  }
+`
 
 class SideMenu extends Component {
   render() {
     const { showSideMenu } = this.props
     return (
       <div className={`SideMenu ${(showSideMenu && "show") || ""}`}>
-        {MENU.map((conf, index) => {
-          const { label, icon, style } = conf
-          const Icon = FontAwesome[icon]
+        {MENU_CONFIG.map((conf, index) => {
+          const { label, iconClass, style, imgUrl } = conf
+          const itemStyle = {
+            ...(imgUrl && {
+              background: `url('${imgUrl}') no-repeat`,
+              backgroundColor: "white",
+              backgroundSize: "100%",
+            }),
+            animationDelay: 0.2 * index + "s",
+            transitionDelay: 0.2 * index + "s",
+          }
+
           return (
-            <div
-              key={label}
-              className={"MenuItem"}
-              style={{ ...style, animationDelay: 0.1 * index + "s" }}>
-              <Icon
-                color="white"
-                style={{
-                  width: "60%",
-                  height: "60%",
-                  top: "50%",
-                  left: "50%",
-                }}
-              />
+            <div key={label} className={"MenuContainer"} style={{ ...style }}>
+              <SmallItem size={600} className={`MenuItem`} style={itemStyle}>
+                {iconClass && (
+                  <a>
+                    <i
+                      className={iconClass}
+                      style={{
+                        fontSize: "1.7rem",
+                      }}
+                    />
+                  </a>
+                )}
+              </SmallItem>
+              <BigItem size={600} className={`MenuItemBig`} style={itemStyle}>
+                <span>
+                  <i
+                    className={iconClass}
+                    style={{
+                      fontSize: "2rem",
+                    }}
+                  />
+                  {label}
+                </span>
+              </BigItem>
             </div>
           )
         })}
