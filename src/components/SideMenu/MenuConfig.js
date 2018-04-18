155cd28b843153674e54328c2508868ada7a1720
Khanh Duc Ha <ken.haduc@gmail.com>
2018-04-18 23:00:56 +1200

    Move menu config to separate file

---
+++ src/components/SideMenu/MenuConfig.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    label: "Profile",
+    imgUrl: "https://robohash.org/ipsumetvoluptas.jpg?size=200x200&set=set1",
+    style: {
+      top: "5%",
+    },
+  },
+  {
+    label: "Friends",
+    iconClass: "fas fa-users",
+    style: {
+      top: "30%",
+    },
+  },
+  {
+    label: "Favorites",
+    iconClass: "fas fa-star",
+    style: {
+      top: "45%",
+    },
+  },
+  {
+    label: "Conversations",
+    iconClass: "fas fa-comment",
+    style: {
+      top: "60%",
+    },
+  },
+  {
+    label: "Settings",
+    iconClass: "fas fa-cog",
+    style: {
+      top: "85%",
+    },
+  },
+]
